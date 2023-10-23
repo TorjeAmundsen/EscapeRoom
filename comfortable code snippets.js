b843ba180d37aa605f1bf406106a059f7ba4dd76
Torje Amundsen <torjefrafen@hotmail.no>
2023-10-23 15:52:41 +0200

    comfy code

---
+++ comfortable code snippets.js
@@ -0,0 +1,38 @@
+/*
+
+Instruksjon: Putt # foran det du ikke er konfortabl med.
+
+    div
+    buttons
+    if-setninger
+    for loops med index
+    for-of loops igjennom liste
+    å kalle bilder fra internett
+    å kalle bilder fra lokalt
+    å bruke ternary ( : ? ) ? = truthy, : = falsy...
+    while
+    getelementsbyclass
+    eval
+    button disable
+    querySelector
+    return-verdier
+    multiline-strings med variabler ( `example ${test} more stuff` )
+    arrow-functions
+    
+    
+    
+    ### JS Classes
+*/
+for (let index = 0; index < array.length; index++) {}
+
+for (bil of biler) {
+}
+
+document.getElementById("app");
+
+document.querySelector("#app"); // id
+document.querySelector(".right-box"); // class
+document.querySelectorAll("button"); // element-type
+document.querySelectorAll(".right-box", ".left-box"); // velg flere typer ting samtidig
+
+eval("updateView()");
